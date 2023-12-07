74e255bdacdc567a177a06124a8415bf053443ee
javed-asif <74103486+javed-asif@users.noreply.github.com>
2023-12-06 20:33:47 -0500

    Update testDDL.sql

--- testDDL.sql
+++ testDDL.sql
@@ -1,1 +1,6 @@
-
+CREATE SCHEMA IF NOT EXISTS test_schema;
+CREATE SEQUENCE IF NOT EXISTS test_schema.testseq;
+CREATE TABLE IF NOT EXISTS test_schema.test_table (id integer, name varchar(100));
+INSERT INTO test_schema.test_table VALUES (nextval('test_schema.testseq'), 'Abc Xyz');
+INSERT INTO test_schema.test_table VALUES (nextval('test_schema.testseq'), 'Abc Xyz');
+INSERT INTO test_schema.test_table VALUES (nextval('test_schema.testseq'), 'Abc Xyz');
