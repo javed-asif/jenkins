a08c6f9f3ada25136e53ad5cc37a6f0d5af13953
javed-asif <74103486+javed-asif@users.noreply.github.com>
2023-12-08 09:06:17 -0500

    Delete createdb.bat

--- createdb.bat
+++
@@ -1,2 +0,0 @@
-echo SELECT 'CREATE DATABASE testdb' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'testdb')\gexec | psql "postgresql://postgres:postgres@192.168.2.110/postgres"
-psql -a -f testDDL.sql "postgresql://postgres:postgres@192.168.2.110/testdb"
